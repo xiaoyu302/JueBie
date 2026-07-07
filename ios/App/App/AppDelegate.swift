aa2752d7c38cf6e783bd0392558d2524494ef316
xiaoyu302 <2078735099@qq.com>
2026-07-08 06:59:00 +0800

    Fix: load index.html from public directory in bundle

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -20,8 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         webView.backgroundColor = UIColor(red: 0.043, green: 0.055, blue: 0.078, alpha: 1.0)
         webView.scrollView.backgroundColor = UIColor(red: 0.043, green: 0.055, blue: 0.078, alpha: 1.0)
         
-        // Load HTML from bundle
-        if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
+        // Load HTML from public folder in bundle
+        if let publicPath = Bundle.main.path(forResource: "index", ofType: "html", inDirectory: "public") {
+            let url = URL(fileURLWithPath: publicPath)
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        } else if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
             let url = URL(fileURLWithPath: htmlPath)
             webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
         }
